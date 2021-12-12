5c44d21a3b1dd209dc6f401f07f686f8e570b7f5
Harton <hartontw@gmail.com>
2021-12-12 16:06:39 +0100

    📜 Better config 🧪 Fixed Jest 🧠 Added Icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { path } from 'path';
+import { basename } from 'path';
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-const base = mode === 'production' ? `/ ${path(process.cwd())} /` : '/';
+const base = mode === 'production' ? `/ ${basename(process.cwd())} /` : '/';
 
 export default {
   root: 'src',
